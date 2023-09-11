8ca1f2554a736763521b62c834f79a5fa5548d6c
alim <alimasudd.dev@gmail.com>
2023-09-11 10:32:23 +0700

    declare variables in QuoteList.kt

--- app/src/main/java/id/alimasudd/rerofit_qoroutine/data/QuoteList.kt
+++ app/src/main/java/id/alimasudd/rerofit_qoroutine/data/QuoteList.kt
@@ -1,4 +1,10 @@
 package id.alimasudd.rerofit_qoroutine.data
 
 data class QuoteList(
+    val count: Int,
+    val lastItemIndex: Int,
+    val page: Int,
+    val results: List<Result>,
+    val totalCount: Int,
+    val totalPages: Int
 )
