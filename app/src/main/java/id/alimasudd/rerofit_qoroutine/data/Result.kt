62bd221b3507664303cc65a67101bea4084544e4
alim <alimasudd.dev@gmail.com>
2023-09-12 10:00:02 +0700

    declare variables in Result.kt

--- app/src/main/java/id/alimasudd/rerofit_qoroutine/data/Result.kt
+++ app/src/main/java/id/alimasudd/rerofit_qoroutine/data/Result.kt
@@ -1,4 +1,12 @@
 package id.alimasudd.rerofit_qoroutine.data
 
 data class Result(
+    val _id: String,
+    val author: String,
+    val authorSlug: String,
+    val content: String,
+    val dateAdded: String,
+    val dateModified: String,
+    val length: Int,
+    val tags: List<String>
 )
