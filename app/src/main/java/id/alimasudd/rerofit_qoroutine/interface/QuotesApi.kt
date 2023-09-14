3fc44a893bd14da9dd3d10dcaba4360876b77572
alimasudd <alimasudd.dev@gmail.com>
2023-09-14 15:48:01 +0700

    create QuotesApi.kt

--- app/src/main/java/id/alimasudd/rerofit_qoroutine/interface/QuotesApi.kt
+++ app/src/main/java/id/alimasudd/rerofit_qoroutine/interface/QuotesApi.kt
@@ -1,4 +1,10 @@
 package id.alimasudd.rerofit_qoroutine.`interface`
 
+import id.alimasudd.rerofit_qoroutine.data.QuoteList
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface QuotesApi {
+    @GET("/quotes")
+    suspend fun getQuotes() : Response<QuoteList>
 }
