affe4aee8120b978915df0d5d17c8bdbad0322c3
alimasudd <alimasudd.dev@gmail.com>
2023-09-14 15:47:36 +0700

    create RetrofitHelper.kt

--- app/src/main/java/id/alimasudd/rerofit_qoroutine/helper/RetrofitHelper.kt
+++ app/src/main/java/id/alimasudd/rerofit_qoroutine/helper/RetrofitHelper.kt
@@ -1,4 +1,17 @@
 package id.alimasudd.rerofit_qoroutine.helper
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitHelper {
+
+    private const val baseUrl = "https://quotable.io/"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            // we need to add converter factory to
+            // convert JSON object to Java object
+            .build()
+    }
 }
