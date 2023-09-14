6d46ce0a30e75d5c6f11263482f550d81201f548
alimasudd <alimasudd.dev@gmail.com>
2023-09-14 15:49:00 +0700

    get instance quoteAPI

--- app/src/main/java/id/alimasudd/rerofit_qoroutine/MainActivity.kt
+++ app/src/main/java/id/alimasudd/rerofit_qoroutine/MainActivity.kt
@@ -2,10 +2,19 @@ package id.alimasudd.rerofit_qoroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import id.alimasudd.rerofit_qoroutine.`interface`.QuotesApi
+import id.alimasudd.rerofit_qoroutine.helper.RetrofitHelper
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
     }
 }
